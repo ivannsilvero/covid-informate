fb001a397c08410c08fb14a95b6712e38858551f
Iván Silvero <ivannsilvero@gmail.com>
2020-06-10 18:11:09 -0300

    Commit

--- public/js/nav.js
+++ public/js/nav.js
@@ -5,9 +5,9 @@ const navSlide = () => {
     const navLinks = document.querySelectorAll('.nav-links li');
 
     burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
+        nav.classList.toggle('.nav-active');
         navLinks.forEach((link, index) => {
-            (link.style.animation) ? link.style.animation = '': link.style.animation = `navLinkFade 0.5s ease forwards ${ index / 7 + 0.5}s`;
+            (link.style.animation) ? link.style.animation = '': link.style.animation = `navLinkFade 0.5s ease forwards ${ index / 7 + 0.5 }s`;
         });
         burger.classList.toggle('.toggle');
     });
