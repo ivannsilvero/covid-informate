a8f0942c57bc6f610999a1d82fae3f1d05bd21fc
Iván Silvero <ivannsilvero@gmail.com>
2020-06-10 12:47:07 -0300

    Commit

---
+++ public/js/contactos.js
@@ -0,0 +1,24 @@
+(() => {
+    'use strict';
+
+    document.querySelectorAll('.copy').forEach(telefono => {
+        telefono.addEventListener('click', () => {
+            const seleccion = window.getSelection();
+            const rango = document.createRange();
+            rango.selectNodeContents(telefono);
+            seleccion.removeAllRanges();
+            seleccion.addRange(rango);
+            document.execCommand('copy');
+            seleccion.removeAllRanges();
+            const original = telefono.textContent;
+            telefono.textContent = 'Copiado!';
+            telefono.classList.add('exito');
+
+            setTimeout(() => {
+                telefono.textContent = original;
+                telefono.classList.remove('exito');
+            }, 1200);
+
+        });
+    });
+})();
